598f669dffdeaa4867974f4c1c9df2dfb4219466
WaywayBao <way879161@gmail.com>
2024-03-25 16:16:08 +0800

    updata risky object annotation

--- realworld/label_tool/get_data.py
+++ realworld/label_tool/get_data.py
@@ -6,7 +6,6 @@ from PIL import Image
 import torchvision
 from pathlib import Path
 
-
 def get_transformation_matrix(R, t, inv=False):
     pose = np.eye(4, dtype=np.float32)
     pose[:3, :3] = R if not inv else R.T
@@ -35,19 +34,21 @@ class nuscenes_dataset_helper:
         return get_pose(record['rotation'], record['translation'], *args, **kwargs)
     
     def parse_sample_record(self, sample_record, camera_rig='CAM_FRONT'):   
-        
+
         sensor_record = self.nusc.get('sample_data', sample_record['data'][camera_rig])
         calibrated_sensor = self.nusc.get('calibrated_sensor', sensor_record['calibrated_sensor_token'])
         ego_sensor = self.nusc.get('ego_pose', sensor_record['ego_pose_token'])
-        world_from_ego = self.parse_pose(ego_sensor)
+        # world_from_ego = self.parse_pose(ego_sensor)
         ego_from_world = self.parse_pose(ego_sensor, inv=True)
         sensor_from_ego = self.parse_pose(calibrated_sensor, inv=True)
         intrinsic = calibrated_sensor['camera_intrinsic']
         extrinsic = sensor_from_ego @ ego_from_world
 
+        bbox = self.get_box_coor(sample_record, sensor_record)
         sample = {'images':sensor_record['filename'],
                   'intrinsics': intrinsic,
-                   'extrinsics': extrinsic}
+                   'extrinsics': extrinsic,
+                   'bbox':bbox}
 
         return sample
 
@@ -89,6 +90,64 @@ class nuscenes_dataset_helper:
 
         return result
 
+
+    def view_points(self, points, view) -> np.ndarray:
+        """
+            This is a helper class that maps 3d points to a 2d plane. It can be used to implement both perspective and
+            orthographic projections. It first applies the dot product between the points and the view. By convention,
+            the view should be such that the data is projected onto the first 2 axis. It then optionally applies a
+            normalization along the third dimension.
+            For a perspective projection the view should be a 3x3 camera matrix, and normalize=True
+            For an orthographic projection with translation the view is a 3x4 matrix and normalize=False
+            For an orthographic projection without translation the view is a 3x3 matrix (optionally 3x4 with last columns
+            all zeros) and normalize=False
+            :param points: <np.float32: 3, n> Matrix of points, where each point (x, y, z) is along each column.
+            :param view: <np.float32: n, n>. Defines an arbitrary projection (n <= 4).
+                The projection should be such that the corners are projected onto the first 2 axis.
+            :param normalize: Whether to normalize the remaining coordinate (along the third axis).
+            :return: <np.float32: 3, n>. Mapped point. If normalize=False, the third coordinate is the height.
+        """
+        assert view.shape[0] <= 4
+        assert view.shape[1] <= 4
+        assert points.shape[0] == 3
+        viewpad = np.eye(4)
+        viewpad[:view.shape[0], :view.shape[1]] = view
+        nbr_points = points.shape[1]
+        # Do operation in homogenous coordinates.
+        points = np.concatenate((points, np.ones((1, nbr_points))))
+        points = np.dot(viewpad, points)
+        points = points[:3, :]
+        points = points / points[2:3, :].repeat(3, 0).reshape(3, nbr_points)
+        return points
+    
+
+    def get_box_coor(self, sample, cam):
+
+        # sample = self.nusc.get('sample', batch['token'][0])
+        # cam = self.nusc.get('sample_data', sample['data']['CAM_FRONT'])
+        poserecord = self.nusc.get('ego_pose', cam['ego_pose_token'])
+        cs_record = self.nusc.get('calibrated_sensor', cam['calibrated_sensor_token'])
+
+        for annotation_token in sample['anns']:
+            
+            # GLOBAL box
+            ann = self.nusc.get('sample_annotation', annotation_token)
+            box = Box(ann['translation'], ann['size'], Quaternion(ann['rotation']))
+            
+            # First step: transform from global into the ego vehicle frame for the timestamp of the image.
+            box.translate(-np.array(poserecord['translation']))
+            box.rotate(Quaternion(poserecord['rotation']).inverse)
+            
+            # Second step: transform from ego into the camera.
+            box.translate(-np.array(cs_record['translation']))
+            box.rotate(Quaternion(cs_record['rotation']).inverse)
+            
+            # Third step: Project to image -> 8 corners points
+            corners = self.view_points(box.bottom_corners(), cs_record['camera_intrinsic'])[:2, :]
+            print(corners)
+
+        return corners
+
     def iter_nusc(self):
         # Iterate dataset's scene
         # Each scene contains multiple sample(frame)
@@ -100,19 +159,19 @@ class nuscenes_dataset_helper:
             while sample_token:
                 sample_record = self.nusc.get('sample', sample_token)
                 sample = self.parse_sample_record(sample_record,'CAM_FRONT')
-                result = nusc.get_cameras(sample)
+                result = self.get_cameras(sample)
                 data.append(result)
                 sample_token = sample_record['next']
 
             all_data.append(data)
-            # break
+            break
 
         return all_data
 
 
 if __name__ == '__main__':
 
-    # nusc = nuscenes_dataset_helper('v1.0-test', "/media/waywaybao_cs10/Disk_2/nuscenes_test")
-    nusc = nuscenes_dataset_helper('v1.0-trainval', "/media/waywaybao_cs10/Disk_2/nuscenes_train")
-    all_data = nusc.iter_nusc()
-    print(len(all_data))
+    # Nusc = nuscenes_dataset_helper('v1.0-test', "/media/waywaybao_cs10/Disk_2/nuscenes_test")
+    Nusc = nuscenes_dataset_helper('v1.0-trainval', "/media/waywaybao_cs10/Disk_2/nuscenes_train")
+    all_data = Nusc.iter_nusc()
+    print("Total #sample:", len(all_data))
--- realworld/label_tool/risky_object/controller.py
+++ realworld/label_tool/risky_object/controller.py
@@ -1,9 +1,8 @@
-from PyQt5 import QtCore
+# from PyQt5 import QtCore
 # from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
-# from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
 from pathlib import Path
-
 import time
 import os
 from collections import OrderedDict
@@ -20,9 +19,12 @@ class MainWindow_controller(QMainWindow):
         self.ui.setupUi(self)
 
         self.video_root = f"../videos"
-        self.behavio_root = f"./annotation"
+        self.behavio_root = f"../behavior/annotation"
+        self.risky_object_root = f"./annotation"
         self.phases = ["train", "test"][:]
-
+        self.actor_id = None
+        self.ro_dict = OrderedDict()
+        
         self.init_phase()
 
         self.ui.pushButton_next.clicked.connect(self.frame_next)
@@ -34,10 +36,41 @@ class MainWindow_controller(QMainWindow):
         self.ui.end_frame_bottom.clicked.connect(self.set_end_frame)
         self.ui.save_non_risky_bottom.clicked.connect(self.save_non_risky)
 
+        self.ui.save_RO_bottom.clicked.connect(self.save_RO)
+        self.ui.add_RO_bottom.clicked.connect(self.add_RO)
+        self.ui.clean_all_bottom.clicked.connect(self.clean_all)
+
         self.ui.comboBox_phase.currentIndexChanged.connect(
             self.init_scenarios)
         self.ui.comboBox_scenario.currentIndexChanged.connect(
             self.update_scenario)
+
+
+    def mousePressEvent(self, event):
+        x = event.pos().x()-220
+        y = event.pos().y()-80
+
+        # TODO
+        self.actor_id = int(x+y)//2
+
+        # if not self.actor_id in gt_id
+        #   self.ui.label_selectedID.setText(f"Select ID : None")
+        #   return
+
+        if event.button() == Qt.LeftButton:
+            # add specific id in buffer
+            start_frame = int(self.ui.start_frame_bottom.text().split()[1])
+            end_frame = int(self.ui.end_frame_bottom.text().split()[1])
+            self.ro_dict[str(self.actor_id)] = [start_frame, end_frame]
+            self.ui.label_selectedID.setText(f"Select ID : {int(self.actor_id):3d}")
+
+        elif event.button() == Qt.RightButton:
+            # delete specific id in all frame
+            if str(self.actor_id) in self.ro_dict:
+                del self.ro_dict[str(self.actor_id)]
+            self.ui.label_selectedID.setText(f"Delete ID : {int(self.actor_id):3d}")
+
+        self.show_ro_list()
 
 
     def init_phase(self):
@@ -57,7 +90,15 @@ class MainWindow_controller(QMainWindow):
         self.video_list = list()
         for video in sorted(os.listdir(self.video_root+f"/{self.phase}")):
             scenario = video.split('.')[0]
+
+            json_path = os.path.join(self.behavio_root, self.phase, scenario+'.json')
+            data = json.load(open(json_path))
+            if list(data.values())[0] == -1:                
+                continue
+
             self.video_list.append(scenario)
+
+        print("Risky scenario :", len(self.video_list))
 
         self.scenario_clear = True
         self.ui.comboBox_scenario.clear()
@@ -72,10 +113,10 @@ class MainWindow_controller(QMainWindow):
 
         if not self.scenario_clear:
             self.current_scenario = self.ui.comboBox_scenario.currentText()
-
             self.update_video()
-            self.load_json()
-
+            self.load_behavior_json()
+            self.load_RO_json()
+            
     def update_video(self):
 
         self.video_path = f"{self.video_root}/{self.phase}/{self.current_scenario}.avi"
@@ -83,7 +124,7 @@ class MainWindow_controller(QMainWindow):
 
         if self.init_video:
             self.video_controller = video_controller(
-                video_path=self.video_path, ui=self.ui)
+                video_path=self.video_path, ui=self.ui, main_contriller=self)
 
             self.ui.button_play.clicked.connect(self.video_controller.play)
             self.ui.button_stop.clicked.connect(self.video_controller.stop)
@@ -92,13 +133,12 @@ class MainWindow_controller(QMainWindow):
         self.video_controller.update_video_path(self.video_path)
 
 
-    def load_json(self):
+    def load_behavior_json(self):
 
         self.ui.label_11.setText(f"Behavior Stop")
-
-        if os.path.exists(f"{self.behavio_root}/{self.phase}/{self.current_scenario}.json"):
-            with open(f"{self.behavio_root}/{self.phase}/{self.current_scenario}.json", 'r') as openfile:
-                data = json.load(openfile)
+        file_path = f"{self.behavio_root}/{self.phase}/{self.current_scenario}.json"
+        if os.path.exists(file_path):
+            data = json.load(open(file_path))
         else:
             data = {
                 "start_frame": -1,
@@ -141,11 +181,62 @@ class MainWindow_controller(QMainWindow):
         # print(set(self.video_list)-set(behavior_dict.keys()))
 
 
+    def load_RO_json(self):
+
+        self.actor_id = None
+        self.ui.label_selectedID.setText(f"Select ID : {self.actor_id}")
+
+        file_path = f"{self.risky_object_root}/{self.phase}/{self.current_scenario}.json"
+        if os.path.exists(file_path):
+            self.ro_dict = json.load(open(file_path))
+        else:
+            self.ro_dict = OrderedDict()
+
+        self.show_ro_list()
+        self.ui.start_lineEdit.setText("")
+        self.ui.end_lineEdit.setText("")
+
+    def show_ro_list(self):
+
+        text = "" if len(self.ro_dict)>0 else "None"
+        for id in self.ro_dict:
+            text += f"{int(id):3d} : {self.ro_dict[id]}\n"
+        self.ui.label_saved_id.setText(text)
+
+    def add_RO(self):
+
+        start_text = self.ui.start_lineEdit.text()
+        end_text = self.ui.end_lineEdit.text()
+
+        if start_text == "" or end_text == "":
+            print("No input frame no. !!!")
+            return
+        elif self.actor_id == None:
+            print("No input actor id !!!")
+            return
+
+        start_frame = int(start_text)
+        end_frame = int(end_text)
+        self.ro_dict[str(self.actor_id)] = [start_frame, end_frame]
+        self.show_ro_list()
+
+    def save_RO(self):
+        with open(f"{self.risky_object_root}/{self.phase}/{self.current_scenario}.json", "w") as f:
+            json.dump(self.ro_dict, f, indent=4)
+    
+    def clean_all(self):
+
+        self.actor_id = None
+        self.ro_dict = OrderedDict()
+        self.show_ro_list()
+        self.ui.label_selectedID.setText(f"Select ID : {self.actor_id}")
+
 
     def frame_next(self):
         # frame no. start with 1
         current_frame_no = self.video_controller.current_frame_no
         current_frame_no = current_frame_no % self.video_controller.video_total_frame_count + 1
+
         self.video_controller.setslidervalue(current_frame_no)
 
     def frame_back(self):
@@ -153,7 +244,9 @@ class MainWindow_controller(QMainWindow):
         current_frame_no = self.video_controller.current_frame_no
         current_frame_no = (
             current_frame_no-2) % self.video_controller.video_total_frame_count + 1
+
         self.video_controller.setslidervalue(current_frame_no)
+
 
     def basic_next(self):
 
